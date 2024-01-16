883a9e8cd6d33db058b3bdb9931327e887cbd459
Qu1nnXD <97952093+Qu1nnXD@users.noreply.github.com>
2024-01-16 09:55:18 +0000

    we gingen lekker testen

--- testing.py
+++ testing.py
@@ -6,14 +6,17 @@ woorden = ["welkom", "galgje", "leugen", "babbelen", "schrijver"]
 woord = random.choice(woorden)
 geradenWoord = ["_"] * len(woord)
 print("Dit is je woord. " + "-" * len(woord))
+run = True
 
 
-
-while geradenWoord != woord:
+while run:
   gok = input("Raad een letter: ")
   if gok in woord:
     print("Goed geraden!")
   for aantalLetters in range(0, len(woord)):
     if gok == woord[aantalLetters]:
       geradenWoord[aantalLetters] = gok
+
+    # als geradenWoord goed is, run is False
+
   print(geradenWoord)
