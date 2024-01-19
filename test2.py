dfae1b4c86980a2f755b9857614bfdd1afcff083
Qu1nnXD <97952093+Qu1nnXD@users.noreply.github.com>
2024-01-19 13:55:59 +0000

    updated wordle en beide test files

--- test2.py
+++ test2.py
@@ -17,30 +17,111 @@ def displayed_word(word, guessed_letters):
         else:
             display += "_"
     return display
+# hangman visual stages
+def draw_hangman():
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |      
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[attempts]
+
 # hangman
-def hangman():
+def hangman(visual_mode = True):
     global attempts
 
     print("Welkom bij Galgje!")
     print(displayed_word(word, guessed_letters))
     
     while True:
-        guess = input("Guess a letter: ")
+        guess = input("Raad een letter: ")
         if guess in guessed_letters:
             print("Die letter heb je al geraden! Probeer opnieuw!")
             continue
         guessed_letters.append(guess)
         if guess == word:
-            print("Goed zo! Wat een topper! Je hebt het woord geraden")
+            print("Supertof! Je hebt het woord in één keer geraden!")
             break
         if guess not in word:
             attempts -= 1
             print(f"Foute gok! Je hebt nog maar {attempts} pogingen!")
         print(displayed_word(word, guessed_letters))
         if "_" not in displayed_word(word, guessed_letters):
-            print("Goed zo! Wat een topper! Je hebt het woord geraden")
+            print("Goed zo! Je hebt het woord geraden!")
             break
+        if visual_mode:
+            print(draw_hangman())
         if attempts == 0:
-            print("Je hebt geen pogingen meer, sorry!")
+            print(f"Je hebt geen pogingen meer, sorry! Het woord was {word}.")
             break
-hangman()         
+visual_mode = input("Wil je met beeld spelen? (ja/nee): ")
+if visual_mode.lower() == "ja":
+    hangman()
+else:
+    hangman(False)   
---
+++ wordle.py
@@ -0,0 +1,48 @@
+# Wordle intro
+
+print("Welkom bij Wordle!")
+print("Het doel van het spel is om het woord te raden.")
+print("Je hebt hier 6 beurten voor")
+print("Het woord bestaat uit 6 letters, dus vul ook een 6 letter lang woord in.")
+print("Veel plezier!")
+
+# woordenlijst
+
+woordenlijst = ["letter", "oranje", "jammer" , "appel" , "hockey" , "voeten" , "galgje" , "school" , "klaver" , "wordle" , "schaap" , "aapjes" , "status" , "cervix" , "piemel" , "school" , "python" , "zwaluw" , "metaal" , "methyl" , "banaan" , "schild" , "ethaan" , "aziaat" , "europa" , "afrika" , "keizer" , "koning" , "gister" , "welkom" , "sirene" , "juiste" , "zwarte" , "racist" , "borden" , "twaalf"]
+
+# Woord kiezen
+
+import random
+woord = random.choice(woordenlijst)
+
+# Woord raden
+
+goed = False
+
+for i in range(6):
+    print(" ")
+    gok = input("Raad het woord: ")
+    print(" ")
+    if gok == woord:
+        print("Gefeliciteerd, je hebt het woord geraden!")
+        goed = True
+        break
+    if len(gok) != 6:
+    else:
+       print("Je moet een woord van 6 letters invoeren.")
+       break
+    for j in range(6):
+        if gok[j] == woord[j]:
+                print(gok[j], "is in het woord en op de juiste plek")
+        elif gok[j] in woord:
+                print(gok[j], "is in het woord maar niet op de juiste plek")
+        else:
+                print(gok[j], "is niet in het woord")
+        if len(gok) != 6:
+                print("Je moet een woord van 6 letters invoeren.")
+                print(" ")
+                break          
+
+if not goed:
+  print(" ")
+  print("Volgende keer beter!")
