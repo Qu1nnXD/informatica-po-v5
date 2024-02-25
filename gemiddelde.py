2b3524ed3aec0c7c5860b0b24a4549ccca64794d
Qu1nnXD <97952093+Qu1nnXD@users.noreply.github.com>
2024-02-25 13:49:41 +0100

    Update gemiddelde.py

--- gemiddelde.py
+++ gemiddelde.py
@@ -1,24 +1,27 @@
+# intro gemiddelde berekenen voor max 4 cijfers
 print("Welkom bij het berekenen van je voldoende! \n")
- 
-cijfers = [float(input("noem een cijfer goezoe: "))]
-weging = [float(input("noem de weging van dit cijfer goezoe: "))]
-cijfers.append(float(input("noem nog een cijfer goezoe: ")))
-weging.append(float(input("noem de weging van dit cijfer goezoe: ")))
-cijfers.append(float(input("noem nog een cijfer goezoe: ")))
-weging.append(float(input("noem de weging van dit cijfer goezoe: ")))
-cijfers.append(float(input("noem nog een cijfer goezoe: ")))
-weging.append(float(input("noem de weging van dit cijfer goezoe: ")))
- 
+# cijfers en wegingen invoeren door middel van lijsten
+cijfers = [float(input("Noem een cijfer: "))]
+weging = [float(input("Noem de weging van dit cijfer: "))]
+cijfers.append(float(input("Noem nog een cijfer: ")))
+weging.append(float(input("Noem de weging van dit cijfer: ")))
+cijfers.append(float(input("Noem nog een cijfer: ")))
+weging.append(float(input("Noem de weging van dit cijfer: ")))
+cijfers.append(float(input("Noem nog een cijfer: ")))
+weging.append(float(input("Noem de weging van dit cijfer: ")))
+# variabelen toewijzen 
 aantalWegingen = len(weging)
 aantalCijfers = len(cijfers)
 totaalCijfers = 0
 totaalWegingen = 0
 gemiddeldeCijfers = []
+# for loop om de wegingen van cijfers correct te gebruiken
 for i in range(0, aantalCijfers):
     gemiddeldeCijfers.append(cijfers[i] * weging[i])
  
- 
+# gemiddelde berekenen en weergeven of dit voldoende is of niet door gebruik te maken van if/else-statements
 if sum(gemiddeldeCijfers) / sum(weging) >= 5.5:
-  print("voldonede goezoe")
+  print("Je staat een voldoende voor dit vak! :D")
 else:
-  print("L")
+  print("Helaas sta je onvoldoende voor dit vak :( ")
+input("Berekenen beëindigd: Druk op Enter om te sluiten...") 
